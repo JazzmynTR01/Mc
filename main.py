b3c4cbb0cd56aa34bab4b7ed3dada2c2d99cc4d6
“JazzmynTR01git config --global user.email “S10257728@connect.np.edu.sg <loljazzmyn@gmail.com>
2023-07-26 23:24:56 +0800

    jazz

--- main.py
+++ main.py
@@ -3,12 +3,10 @@ from pathlib import Path
 # import final results as module in main.py
 from overheads import summary_overheads as oh_result
 from coh import result as coh_result
-<<<<<<< HEAD
 from profit_loss import result as pl_result
-=======
 from profit_loss import pl
 from profit_loss import result as p1_result
->>>>>>> 05c0e97cc4ef18624c2ef97fbb2bf6026d4ff68d
+
 
 fp_write = Path.cwd()/"summary_report.txt"
 # create the text file
